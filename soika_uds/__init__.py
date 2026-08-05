b667c1454968794f55036c101fa9ca42039102bb
sergey-kznetsov <123492323+sergey-kznetsov@users.noreply.github.com>
2026-08-05 12:31:10 +0400

    Expose contract v1 from public package

--- soika_uds/__init__.py
+++ soika_uds/__init__.py
@@ -11,21 +11,55 @@ from .contracts import (
     TerritoryContext,
 )
 from .engine import SoikaEngine
+from .integration import (
+    CURRENT_CONTRACT_VERSION,
+    SUPPORTED_CONTRACT_VERSIONS,
+    AnalysisRequestV1,
+    AnalysisResultV1,
+    ContractIssue,
+    ContractValidationError,
+    ContractVersion,
+    IdempotencyConflictError,
+    JobStatusV1,
+    MessageType,
+    ResultProvenance,
+    assert_idempotent_request,
+    contract_info,
+    export_schema_bundle,
+    parse_contract_document,
+    schema_bundle_digest,
+)
 from .prediction import Prediction, PredictionFormatError
 
-__version__ = "0.2.0"
+__version__ = "0.4.0"
 
 __all__ = [
+    "CURRENT_CONTRACT_VERSION",
+    "SUPPORTED_CONTRACT_VERSIONS",
+    "AnalysisRequestV1",
     "AnalysisResult",
+    "AnalysisResultV1",
+    "ContractIssue",
+    "ContractValidationError",
+    "ContractVersion",
     "CoverageSummary",
+    "IdempotencyConflictError",
     "JobStatus",
+    "JobStatusV1",
     "MessageClassification",
+    "MessageType",
     "ModelResult",
     "PrecisionLevel",
     "Prediction",
     "PredictionFormatError",
+    "ResultProvenance",
     "SoikaEngine",
     "SourceMessage",
     "TerritoryContext",
+    "assert_idempotent_request",
+    "contract_info",
+    "export_schema_bundle",
+    "parse_contract_document",
+    "schema_bundle_digest",
     "__version__",
 ]
