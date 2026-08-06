33885d285a3aafd003ac0836024a0ec063fded49
sergey-kznetsov <123492323+sergey-kznetsov@users.noreply.github.com>
2026-08-06 08:53:58 +0300

    Export classification evaluation API

--- soika_uds/classification/__init__.py
+++ soika_uds/classification/__init__.py
@@ -1,6 +1,12 @@
 """Production classification and topic-refinement public API."""
 
 from .backend import TransformersPredictionBackend
+from .evaluation import (
+    ValidationLabel,
+    evaluate_predictions,
+    label_distribution,
+    total_variation_drift,
+)
 from .models import (
     ClassificationBatchResult,
     ClassificationConfig,
@@ -29,5 +35,9 @@ __all__ = [
     "ModelDescriptor",
     "PredictionBackend",
     "TransformersPredictionBackend",
+    "ValidationLabel",
+    "evaluate_predictions",
+    "label_distribution",
     "load_classification_registry",
+    "total_variation_drift",
 ]
