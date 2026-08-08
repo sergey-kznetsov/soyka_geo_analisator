4b0d88f22969a0909348aaa1d55b22765119e869
sergey-kznetsov <123492323+sergey-kznetsov@users.noreply.github.com>
2026-08-08 09:39:26 +0300

    Version storage worker migration support

--- geoanalyzer_storage/__init__.py
+++ geoanalyzer_storage/__init__.py
@@ -22,7 +22,7 @@ from .postgres import PostgresDatabase, PostgresSettings
 from .registry import ApplicationRegistration, PostgresApplicationRegistry
 from .retention import RetentionManager, RetentionPolicy
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 __all__ = [
     "ApplicationRegistration",
